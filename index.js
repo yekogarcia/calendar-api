49dff0f57202c3ea87d80f0b2f7fc819641ebe7e
yekogarcia <yekogarcia@yahoo.com>
2022-02-28 23:33:16 -0500

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+require('dotenv').config();
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
-require('dotenv').config();
 
 //Crear el servidor express
 const app = express();
@@ -26,6 +26,6 @@ app.use('/api/events', require('./routes/events'));
 
 
 app.listen(process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto 4000');
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
 
