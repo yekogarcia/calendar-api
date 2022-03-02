b2abd506a2e94d2224527ef7d73515cfe81ff946
yekogarcia <yekogarcia@yahoo.com>
2022-03-01 20:56:13 -0500

    cambios

--- database/config.js
+++ database/config.js
@@ -8,6 +8,7 @@ const dbConnection = async () => {
             useCreateIndex: true
         });
         console.log("DB online");
+        
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de incializar DB');
