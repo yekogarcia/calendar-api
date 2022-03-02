2c856655f23d9e48bb1fafb25a4d528992ace91e
yekogarcia <yekogarcia@yahoo.com>
2022-03-01 20:42:54 -0500

    cambios en config

--- database/config.js
+++ database/config.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
         console.log("DB online");
     } catch (error) {
