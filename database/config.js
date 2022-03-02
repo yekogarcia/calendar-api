c6729aa631cd337c4510f9a942548fab2aa8ef97
yekogarcia <yekogarcia@yahoo.com>
2022-03-01 21:49:16 -0500

    ...

--- database/config.js
+++ database/config.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
+            autoIndex: false, // Don't build indexes
+            maxPoolSize: 10, // Maintain up to 10 socket connections
+            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            family: 4 // Use IPv4, skip trying IPv6
         });
-        
+
         console.log("DB online");
-        
+
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de incializar DB');
