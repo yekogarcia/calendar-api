b01d58fb3abc8cf177db6d681ce652ff867677ec
yekogarcia <yekogarcia@yahoo.com>
2022-03-01 22:48:49 -0500

    cambios

--- database/config.js
+++ database/config.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_CNN, {
-            autoIndex: false, // Don't build indexes
-            maxPoolSize: 10, // Maintain up to 10 socket connections
-            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-            family: 4 // Use IPv4, skip trying IPv6
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+            // autoIndex: false, // Don't build indexes
+            // maxPoolSize: 10, // Maintain up to 10 socket connections
+            // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            // family: 4 // Use IPv4, skip trying IPv6
         });
 
         console.log("DB online");
